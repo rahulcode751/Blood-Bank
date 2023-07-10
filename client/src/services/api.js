cfaa35ce967e823bc6c45ab8cb4e1df5cc48fc80
Rahul Bairagi <rahulbairagiofficial@gmail.com>
2023-07-10 22:16:22 +0530

    client deploy

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const url = "http://localhost:8080/api/v1";
+// const url = "http://localhost:8080/api/v1";
+const url = "my-blood-bank.vercel.app";
 // const API = axios.create({ baseURL: process.env.REACT_APP_BASEURL });
 const API = axios.create({ baseURL: url });
 
