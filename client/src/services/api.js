8b3aef497347c6fce59ec612ae78035bac0a5b04
Rahul Bairagi <rahulbairagiofficial@gmail.com>
2023-07-10 23:31:22 +0530

    backend deploy error resolved

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const url = "http://localhost:8080/api/v1";
-// const url = "my-blood-bank.vercel.app";
+// const url = "http://localhost:8080/api/v1";
+const url = "https://mybloodbank.onrender.com";
 // const API = axios.create({ baseURL: process.env.REACT_APP_BASEURL });
 const API = axios.create({ baseURL: url });
 
